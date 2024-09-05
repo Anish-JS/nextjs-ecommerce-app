0cf253b474e67ed212fa6f048e86e2884fe36c42
AnishJakka <221910320002@gitam.in>
2024-09-05 16:43:01 -0700

    Checked authoptions

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,8 +4,10 @@ import { env } from "@/app/lib/env";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { NextAuthOptions } from "next-auth";
 import { Adapter } from "next-auth/adapters";
-import NextAuth from "next-auth/next";
+
 import GoogleProvider from "next-auth/providers/google";
+import NextAuth from "next-auth";
+// import authOptions from "../../../../lib/configs/auth/authOptions";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db) as Adapter,
